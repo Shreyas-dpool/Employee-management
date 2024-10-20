0a1e8cc742f4783d468f9ef34505788840a60ca5
Ryoo3869 <m0un4pr4s4d@gmail.com>
2024-10-20 20:19:11 +0530

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ Cursor.execute('USE Company;')
 
 #Creating tables
 #Mouna insert queries here
-Cursor.execute('CREATE TABLE EMPLOYEES' \                    #Creating Employees tables
+Cursor.execute('CREATE TABLE EMPLOYEES' \                    #Creating Employees table
                '(Id int not null primary key,' \ 
                'Pass varchar(16),' \
                'Name varchar(20) not null,' \
@@ -18,11 +18,20 @@ Cursor.execute('CREATE TABLE EMPLOYEES' \                    #Creating Employees
                'Phone_No int not null unique);')
 
 Cursor.execute('CREATE TABLE PAYSLIPS' \ 
-               '(Id int FOREIGN KEY REFRENCES EMPLOYEES(Id),' \      #Creating Payslips tables
+               '(Id int FOREIGN KEY REFRENCES EMPLOYEES(Id),' \      #Creating Payslips table
                'Base_Amount decimal(10,2),' \
                'Bonus decimal(10,2),' \
                'Deductions decimal (10,2),' \
                'Net_Salary decimal(10,2));' )
+
+Cursor.execute('CREATE TABLE LEAVE' \
+               'Id int FOREIGN KEY REFRENCES EMPLOYEES(Id),' \        #Creating Leave table
+               'Leave varchar(30),' \
+               'Start_Date date,' \
+               'End_Date date,' \
+               'Status date,' \
+               'Reason varchar(300));') 
+
 
 
       
