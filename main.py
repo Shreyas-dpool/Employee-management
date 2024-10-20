d3c819a1b0a17af03ed31fc3fa0275ca9e24e03c
Shreyas. <67584258+Shreyas-dpool@users.noreply.github.com>
2024-10-20 19:47:48 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -3,7 +3,10 @@ import mysql.connector as mysql
 mycon = mysql.connect(host='localhost',user='root',passwd='root')
 # Creating database
 Cursor = mycon.cursor()
-Cursor.execute('CREATE DATABASE if not exists Employee;')
-Cursor.execute('USE Employee;')
+Cursor.execute('CREATE DATABASE if not exists Company;')
+Cursor.execute('USE Company;')
+
+#Creating tables
+#Mouna insert queries here
 
 
